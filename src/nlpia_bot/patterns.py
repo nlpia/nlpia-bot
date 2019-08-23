d195b690f3f3720c555af99a6fa2b5111204518d
hobs <hobs+github@totalgood.com>
2019-08-22 19:42:52 -0700

    revert patterns.py

--- src/nlpia_bot/patterns.py
+++ src/nlpia_bot/patterns.py
@@ -2,24 +2,20 @@
 import re
 
 
-from .template_generators import generate_sentence  # noqa
+def greeting(statement):
+    """ Chatbot "main" function to respond to a user command or statement
 
-
-class Bot:
-    def reply(self, statement):
-        """ Chatbot "main" function to respond to a user command or statement
-
-        >>> respond('Hi')
-        Hi!
-        """
-        responses = []
-        match = re.match(r'\b(hi|hello|hey)\b(.*)', statement.lower())
-        if match:
-            responses.append((0.1, "Hello"))
-            if 'mycroft' in match.groups()[1].lower():
-                responses.append((0.2, "Hi!"))
-                responses.append((0.2, "Hi! What would you like to talk about?"))
-                responses.append((0.2, "Hi! You remembered me! How are you doing?"))
-            if 'bot' in match.groups()[1].lower():
-                responses.append((0.2, "Hey. That's a good one."))
-        return responses
+    >>> respond('Hi')
+    Hi!
+    """
+    responses = []
+    match = re.match(r'\b(hi|hello|hey)\b(.*)', statement.lower())
+    if match:
+        responses.append((0.1, "Hello"))
+        if 'mycroft' in match.groups()[1].lower():
+            responses.append((0.2, "Hi!"))
+            responses.append((0.2, "Hi! What would you like to talk about?"))
+            responses.append((0.2, "Hi! You remembered me! How are you doing?"))
+        if 'bot' in match.groups()[1].lower():
+            responses.append((0.2, "Hey. That's a good one."))
+    return responses
