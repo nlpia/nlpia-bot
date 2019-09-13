7892f61dff795c2037a77947683ad4370bcdd95d
hobs <hobs+github@totalgood.com>
2019-09-13 11:58:14 -0700

    ChatForm in tests.py
    Former-commit-id: 3955f8b12bb83ab273b2cf1fd5d916b9ed17fffe

--- nlpia_bot/urls.py
+++ nlpia_bot/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('bot.urls'), kwargs=dict(username='hobs')),
+    path('chat/', include('bot.urls'), kwargs=dict(username='hobs')),
     path('admin/', admin.site.urls),
 ]
