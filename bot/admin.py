39f7e7e6247d7bac5cbf38aba494a093df810518
hobs <hobs+github@totalgood.com>
2019-09-25 18:53:01 -0700

    finish note->notefile refactor

--- bot/admin.py
+++ bot/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Note
+from .models_notes import NoteFile
 
-admin.site.register(Note)
+admin.site.register(NoteFile)
--- bot/migrations/0001_initial.py
+++ bot/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Note',
+            name='NoteFile',
             fields=[
                 ('id', models.AutoField(primary_key=True, serialize=False)),
                 ('accessed', models.TextField(blank=True, null=True)),
