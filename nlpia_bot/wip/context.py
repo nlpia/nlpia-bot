9a25399b7baab4e69214397db9df046524e484d6
hobs <hobs+github@totalgood.com>
2019-10-30 15:52:25 -0700

    add skills, wip, actions, and plugins dirs
    Former-commit-id: dc94cbbd25fc835038c033183f4dd5c4bfb2eed2

---
+++ nlpia_bot/wip/context.py
@@ -0,0 +1,6 @@
+"""Metrics and search algorithms for context knowledge graphs
+
+>>> score({'thread': 'learn',
+...        'human': {'bash': .5, 'ide': {'vscode': .2, 'sublime': .5}},
+...        'previous': [])
+"""
