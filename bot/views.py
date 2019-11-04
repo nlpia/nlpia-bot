d0f56dfa6a0543b64f8bcf018a98e2ef8e03bdf8
hobs <hobs+github@totalgood.com>
2019-11-03 22:48:00 -0800

    time_bots

--- bot/views.py
+++
@@ -1,6 +0,0 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def webchat(request, username='human'):
-    return HttpResponse(f"Hi {username}! What's on your mind?")
