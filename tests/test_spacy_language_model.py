1f43b81ff757b98ee393683b53268b22ce01adf6
hobs <hobs+github@totalgood.com>
2019-11-23 15:45:56 -0800

    test spacy instead of clibot

--- tests/test_spacy_language_model.py
+++ tests/test_spacy_language_model.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 import pytest  # noqa
 
-from nlpia_bot.clibot import CLIBot
+from nlpia_bot.spacy_language_model import nlp
 
 __author__ = "SEE AUTHORS.md"
 __copyright__ = "Hobson Lane"
 __license__ = "The Hippocratic License"
 
 
-def test_clibot():
-    clibot = CLIBot()
-    assert callable(clibot)
+def test_spacy_language_model():
+    assert callable(nlp)
+    assert len(list(nlp("Hello world!"))) == 3
