7ca1b7f1be73097b9b35e87fac379219d2d6dcdf
hobs <hobs+github@totalgood.com>
2019-11-23 16:01:29 -0800

    clitest is back

---
+++ tests/test_clibot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest  # noqa
+
+from nlpia_bot.clibot import CLIBot
+
+__author__ = "SEE AUTHORS.md"
+__copyright__ = "Hobson Lane"
+__license__ = "The Hippocratic License"
+
+
+def test_clibot():
+    clibot = CLIBot()
+    assert callable(clibot)
