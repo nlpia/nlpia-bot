644c6f3645c78ab7e5174c30273a955e3c80def8
hobs <hobs+github@totalgood.com>
2019-12-09 20:33:55 -0800

    add-clibot-doctest

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -1,7 +1,9 @@
 # test_doctests
 import pytest  # noqa
 import doctest
+
 import nlpia_bot.skills.eliza_bots
+import nlpia_bot.clibot
 
 __author__ = "SEE AUTHORS.md"
 __copyright__ = "Hobson Lane"
@@ -10,6 +12,13 @@ __license__ = "The Hippocratic License, see LICENSE.txt (MIT + Do no Harm)"
 
 def test_eliza_bots():
     results = doctest.testmod(nlpia_bot.skills.eliza_bots, optionflags=doctest.ELLIPSIS |
+                              doctest.NORMALIZE_WHITESPACE, verbose=True)
+    assert results.failed < 1
+    assert results.attempted > 0
+
+
+def test_clibot():
+    results = doctest.testmod(nlpia_bot.clibot, optionflags=doctest.ELLIPSIS |
                               doctest.NORMALIZE_WHITESPACE, verbose=True)
     assert results.failed < 1
     assert results.attempted > 0
