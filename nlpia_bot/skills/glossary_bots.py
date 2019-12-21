fd9c7087fa0f2f0062b521840d3394c41330407e
hobs <hobs+github@totalgood.com>
2019-12-20 23:05:42 -0600

    added more versatile acronym looking to glossary and capitalization synonyms

--- nlpia_bot/skills/glossary_bots.py
+++ nlpia_bot/skills/glossary_bots.py
@@ -1,13 +1,18 @@
 """ Pattern and template based chatbot dialog engines """
 import re
+import logging
 
 import pandas as pd
 
 from nlpia_bot.etl import glossaries
 from nlpia_bot import spacy_language_model
 
-
+log = logging.getLogger(__name__)
 nlp = spacy_language_model.load('en_core_web_md')
+
+
+def capitalizations(s):
+    return (s, s.lower(), s.upper(), s.title())
 
 
 class Bot:
@@ -31,6 +36,14 @@ class Bot:
         self.vector['term'] = pd.DataFrame({s: nlp(s or '').vector for s in self.glossary['term']})
         self.vector['definition'] = pd.DataFrame({s: nlp(s or '').vector for s in self.glossary['definition']})
 
+        self.synonyms = {}
+        # create reverse index of synonyms to canonical terms
+        for term, row in self.glossary.iterrows():
+            self.synonyms.update(dict(zip(capitalizations(term), [term] * 4)))
+            if type(row['acronym']) == str and row['acronym'].strip():
+                acro = row['acronym'].strip()
+                self.synonyms.update(dict(zip(capitalizations(acro), [term] * 4)))
+
     def reply(self, statement):
         """ Suggest responses to a user statement string with [(score, reply_string)..]
 
@@ -44,10 +57,13 @@ class Bot:
         responses = []
         match = re.match(r"\b(what\s+(is|are)\s*(not|n't)?\s+(a|an|the))\b([^\?]*)(\?*)", statement.lower())
         if match:
-            try:
-                responses.append((1, self.glossary['definition'][match.groups()[-2].strip().lower()]))
-            except KeyError:
-                responses.append((1, str(match.groups())))
+            log.info(str(match.groups()))
+            for i, term in enumerate(capitalizations(match.groups()[-2])):
+                normalized_term = self.synonyms.get(term, None)
+                if normalized_term:
+                    responses.append((1 - .02 * i, self.glossary['definition'][normalized_term]))
         else:
-            responses = [(1.0, "I don't understand")]
+            responses = [(0.05, "I don't understand. That doesn't sound like a question I can answer using my glossary.")]
+        if not len(responses):
+            responses.append((0.25, "My glossaries and dictionaries don't seem to contain that term."))
         return responses
