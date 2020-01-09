f1b53cdf08e29d1919edfee56d2dfdce02c99f2d
hobs <hobs+github@totalgood.com>
2020-01-08 21:48:19 -0800

    doctest.testmod(glossaries)
    Former-commit-id: 49aebc956b2f6355d4b11741e4296129380e9d10 [formerly bad4d1a5bf014fcf82c6d4f272737b9da5a58d23]
    Former-commit-id: 65cbb175c250b351a8649a5ec97f7036f345637c

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -4,7 +4,7 @@ import doctest
 
 import nlpia_bot.skills.eliza_bots
 import nlpia_bot.clibot
-import nlpia_bot.etl.test_glossaries
+import nlpia_bot.etl.glossaries
 
 
 __author__ = "SEE AUTHORS.md"
@@ -27,7 +27,7 @@ def test_clibot():
 
 
 def test_glossaries():
-    results = doctest.testmod(nlpia_bot.glossaries, optionflags=doctest.ELLIPSIS |
+    results = doctest.testmod(nlpia_bot.etl.glossaries, optionflags=doctest.ELLIPSIS |
                               doctest.NORMALIZE_WHITESPACE, verbose=True)
     assert results.failed < 1
     assert results.attempted > 5
