b9078e5b699687ab77c47e48a52619d0c0ddb630
hobs <hobs+github@totalgood.com>
2020-01-22 18:44:58 -0800

    modularize training script

--- nlpia_bot/qa_train.py
+++ nlpia_bot/qa_train.py
@@ -1,47 +1,62 @@
 from simpletransformers.question_answering import QuestionAnsweringModel
 import json
 import os
+import logging
+
+from tqdm import tqdm
 
 from nlpia_bot.constants import DATA_DIR
+log = logging.getLogger(__name__)
+
 
 SQUAD_TRAINING_JSON = os.path.join(DATA_DIR, 'simple-transformers', 'train-v2.0.json')
 ST_TRAINING_JSON = os.path.join(DATA_DIR, 'simple-transformers', 'squad-train.json')
 
 
-def train_squad(path=SQUAD_TRAINING_JSON):
-    return path
+def preprocess_squad(squad_path=SQUAD_TRAINING_JSON):
+    """ Load raw SQUAD training set v2.0 from Stanford format json and convert to simpletransformer format
+
+    Cache the simpletransformer json format, if it doesn't yet exist in data/simple-transformer
+    or the provided path
+    """
+    with open(squad_path, 'r') as fin:
+        squad_train = json.load(fin)
+    log.info(f"Loaded {len(squad_train['data'])} examples from {squad_path} dataset...")
+    return [item for topic in tqdm(squad_train['data']) for item in topic['paragraphs']]
 
 
-with open(SQUAD_TRAINING_JSON, 'r') as fin:
-    squad_train = json.load(fin)
+def load_trainset(train_path=ST_TRAINING_JSON, squad_path=SQUAD_TRAINING_JSON):
+    """ Load a preprocessed SQUAD training set into RAM as a list or create it using the raw squad dataset """
+    try:
+        with open(train_path, 'r') as fin:
+            trainset = json.load(fin)
+    except (IOError, FileNotFoundError):
+        trainset = preprocess_squad(squad_path)
+        with open(train_path, 'w') as fout:
+            json.dump(trainset, fout)
+    return trainset
 
-squad_train = [item for topic in squad_train['data'] for item in topic['paragraphs']]
 
-# Save as a JSON file
-with open(ST_TRAINING_JSON, 'w') as fout:
-    json.dump(squad_train, fout)
+def train_squad(train_path=ST_TRAINING_JSON, squad_path=SQUAD_TRAINING_JSON):
+    trainset = load_trainset(train_path, squad_path=squad_path)
+    model = QuestionAnsweringModel('distilbert', 'distilbert-base-uncased-distilled-squad',
+                                   args={'reprocess_input_data': True, 'overwrite_output_dir': True})
+    model.train_model(trainset)
+
+    return model
 
 
-# Create the QuestionAnsweringModel
-model = QuestionAnsweringModel('distilbert', 'distilbert-base-uncased-distilled-squad',
-                               args={'reprocess_input_data': True, 'overwrite_output_dir': True})
+def evaluate(model, test_path=ST_TRAINING_JSON):
+    # Evaluate the model. (Being lazy and evaluating on the train data itself)
+    trainset = load_trainset(test_path)
 
-# Train the model with JSON file
-model.train_model(ST_TRAINING_JSON)
+    result, text = model.eval_model(trainset)
 
-# The list can also be used directly
-# model.train_model(train_data)
+    log.info(result)
+    log.info(text)
+    return result, text
 
-# Evaluate the model. (Being lazy and evaluating on the train data itself)
-result, text = model.eval_model(ST_TRAINING_JSON)
 
-print(result)
-print(text)
-
-print('-------------------')
-
-# Making predictions using the model.
-to_predict = [{'context': 'This is the context used for demonstrating predictions.',
-               'qas': [{'question': 'What is this context?', 'id': '0'}]}]
-
-print(model.predict(to_predict))
+if __name__ == '__main__':
+    model = train_squad()
+    result, text = evaluate(model)
