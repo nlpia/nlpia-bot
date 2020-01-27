bd2deb0a9810a6f4610dc7651742bdc55c23d607
travis-harper <travisdanharper@gmail.com>
2020-01-26 16:43:23 -0800

    fixed qa_bots model loading and caching bugs in addition to silencing transformers logging (warn: weights will not download automatically)
    Former-commit-id: 1b082c5d6264c5c3cd6df9a8671034931cd172c6

--- nlpia_bot/skills/qa_bots.py
+++ nlpia_bot/skills/qa_bots.py
@@ -1,7 +1,9 @@
 """ Transformer based chatbot dialog engine for answering questions """
+
+import logging
 import os
 import uuid
-import logging
+from multiprocessing import cpu_count
 
 from simpletransformers.question_answering import QuestionAnsweringModel
 
@@ -13,13 +15,26 @@ log = logging.getLogger(__name__)
 
 class Bot:
 
-    def __init__(self, path=os.path.join(DATA_DIR, 'simple-transformers')):
-        #self.transformer_loggers = []
-        #for name in logging.root.manager.loggerDict:
-        #    if len(name) >= 12 and name[:12] == 'transformers':
-        #        self.transformer_loggers.append(logging.getLogger(name))
-        #        self.transformer_loggers[-1].setLevel(logging.ERROR)
-        self.model = QuestionAnsweringModel('bert', path, use_cuda=USE_CUDA)
+    def __init__(self):
+        self.transformer_loggers = []
+        for name in logging.root.manager.loggerDict:
+            if len(name) >= 11 and name[:11] in ['transformer', 'simpletrans']:
+                self.transformer_loggers.append(logging.getLogger(name))
+                self.transformer_loggers[-1].setLevel(logging.ERROR)
+
+        process_count = cpu_count() - 2 if cpu_count() > 2 else 1
+        model_path = os.path.join(DATA_DIR, 'simple-transformer')
+        args = {
+            'process_count': process_count,
+            'output_dir': model_path,
+            'cache_dir': model_path,
+            'no_cache': True,
+            'use_cached_eval_features': False,
+            'overwrite_output_dir': False,
+            'silent': True
+        }
+    
+        self.model = QuestionAnsweringModel('bert', model_path, args=args, use_cuda=USE_CUDA)
 
     def encode_input(self, statement, context):
         encoded = [{
@@ -35,23 +50,12 @@ class Bot:
         return output[0]['answer']
 
     def reply(self, statement):
-        response = ''
-        docs = scrape_wikipedia.scrape_article_texts(statement)
+        responses = []
+        docs = scrape_wikipedia.scrape_article_texts()
         for context in docs:
-            heading = context.split('\n')[0]
-            log.info(f'Reading up on {heading}')
             encoded_input = self.encode_input(statement, context)
             encoded_output = self.model.predict(encoded_input)
             decoded_output = self.decode_output(encoded_output)
             if len(decoded_output) > 0:
-                response = response + decoded_output + ' . . .\n'
-                break
-        return [(max(len(response), 1), response.rstrip())]
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+                responses.append((1, decoded_output))
+        return responses
