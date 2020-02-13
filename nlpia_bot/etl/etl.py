afd65159a0c737fb915179f2cc113a27a164704e
Olesya Bondarenko <ovbondarenko@gmail.com>
2020-02-12 21:06:10 -0800

    Add basic entry and query functions for elasticsearch
    Former-commit-id: c9604a18821fde784c64723b961ce9b305ca06e0

--- nlpia_bot/etl/etl.py
+++ nlpia_bot/etl/etl.py
@@ -1,5 +1,62 @@
+import os
+from elasticsearch import Elasticsearch
+
+client = Elasticsearch("http://localhost:9200")
+
 def index_dir(path=os.path.expanduser('~')):
+
     paths = []
-    # for p in walk(path):
-    #     add_file_to_elastic_search(p)
-    return len(paths)
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.py' in file:
+                paths.append(os.path.join(r, file))
+    
+    return paths, len(paths)
+
+
+class Document:
+    
+    def __init__(self, index, id, source):
+        self.index = index
+        self.id = id
+        self.source = source
+        
+        self.json = {}
+        
+        try:
+            
+            self.json = {
+                "_index": self.index,
+                "_id": self.id,
+                "doc_type":"_doc",
+                "_source": self.source,
+            }
+            print("Elasticsearch Document JSON:", self.json)
+            
+        except Exception as error:
+            print("Document JSON instance error: ", error)
+
+# Example document entry
+doc = Document(index = "chatbot", 
+               id =1, 
+               source = {'author1': 'Louis',
+                        'author2': 'Clark'})
+
+def insert_document(document):
+
+    try:
+        response = client.index(doc.index, body=doc.source, id=doc.id)
+        print ("Document index() response:", response, "n")
+    except Exception as error:
+        print ("client.index() ERROR:", error, "n")
+
+
+def query_document(index="chatbot",id=1):
+
+    try:
+        response = client.get(index = index, id=id)
+        print ("Document index() response:", response, "n")
+    except Exception as error:
+        print ("client.index() ERROR:", error, "n")
+
