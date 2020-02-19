7b0bd65ecc9a2362398aa5bf70a80abbba59c616
chalkley <chalkley@ohsu.edu>
2020-02-18 18:03:44 -0800

    add class for vec management. sample wiki titles qary/etl/fileutils blank line removed

--- qary/etl/vectors.py
+++ qary/etl/vectors.py
@@ -1,18 +1,17 @@
-from qary.constants import DATA_DIR
+from nlpia_bot.constants import DATA_DIR
 import os
 import pandas as pd
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics.pairwise import cosine_similarity
 
-from qary.spacy_language_model import load
 import logging
 log = logging.getLogger(locals().get('__name__', ''))
 
+from nlpia_bot.spacy_language_model import load
 nlp = load('en_core_web_md')
 
-default_vector_file = os.path.join(DATA_DIR, 'wikipedia-title-vectors_sample.csv')
-
+default_vector_file=os.path.join(DATA_DIR, 'wikipedia-title-vectors_sample.csv')
 
 def phrase_to_vec(phrase):
     ''' transform doc to vector via nlp model. Returns [float]
@@ -25,11 +24,10 @@ def phrase_to_vec(phrase):
         log.error(f'BAD VEC: {phrase} [0]*{mask_zeros.sum()}')
     return list(vec)
 
-
 class vector_collection():
     def __init__(self, filename=default_vector_file):
-        self.vector_df = pd.read_csv(filename).set_index('index')
-
+        self.vector_df=pd.read_csv(filename).set_index('index')
+    
     def add_phrase(self, phrase):
         vec = phrase_to_vec(phrase)
         self.vector_df.append(pd.DataFrame(vec, index=[phrase]))
@@ -39,8 +37,19 @@ class vector_collection():
         print('similarities\n', sims, sims.shape)
         assert sims.shape == (len(self.vector_df), 1), "Distance calculation error"
 
-        arg = np.argmax(sims)
+        arg= np.argmax(sims)
 
         nearest = self.vector_df.iloc[arg]
         log.info(f'Match found with cosim of {sims[arg]}.')
         return nearest
+
+#  class vector_cluster(size, factor=10):
+#     self.size=size
+#     self.factor=factor
+#     self.root_vec = [0*size]
+#     self.children = []
+    
+#     def add_vectors(self, vectors):
+#         n = len(vectors[0])
+#         n_clusters= n//self.factor
+#         labels = AgglomerativeClustering(n_clusters=n_clusters).fit_predict()
