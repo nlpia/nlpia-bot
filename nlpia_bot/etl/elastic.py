1652839c7983d18e18cd5368748c066076e14521
Olesya Bondarenko <ovbondarenko@gmail.com>
2020-02-20 22:14:56 -0800

    add simple search function
    Former-commit-id: 12d5375d84817290c399fdedd002778ac9145cfc

--- nlpia_bot/etl/elastic.py
+++ nlpia_bot/etl/elastic.py
@@ -7,7 +7,7 @@ from slugify import slugify
 client = Elasticsearch("http://localhost:9200")
 
 ''' 
-Search and scrape wikipedia articles by category
+Search and scrape wikipedia articles from a chosen category
 
 '''
 
@@ -22,19 +22,24 @@ cat = wiki_wiki.page("Category:Natural_language_processing")
 print("Category members: Category:Natural_language_processing")
 print_categorymembers(cat.categorymembers)
 
-# Save the articles in separate .txt files
+# Save articles in separate .txt files
 
-def save_articles(path = 'C:\PythonPrograms\Jupyter\elasticsearch_test\wikipedia_articles', wiki_dict = cat.categorymembers):
+def save_articles(path = 'C:\data\wikipedia_articles', wiki_dict = cat.categorymembers):
     for key, value in wiki_dict.items():
-        page = .page(key)
+        page = wiki_wiki.page(key)
         slug = slugify(key)
         
-        with open(f'{path}\{slug}.txt', 'w') as new_file:
-            new_file.write(page.title)
-            new_file.write('\n')
-            new_file.write(page.fullurl)
-            new_file.write('\n')
-            new_file.write(page.text)
+        try:
+        
+            with open(f'C:\PythonPrograms\Jupyter\elasticsearch_test\wikipedia_articles\{slug}.txt', 'w') as new_file:
+                new_file.write(page.title)
+                new_file.write('\n')
+                new_file.write(page.fullurl)
+                new_file.write('\n')
+                new_file.write(page.text)
+                
+        except Exception as error:
+            print(f"Error writing document {page.title}: ", error)
 
 
 
@@ -44,7 +49,7 @@ def index_dir(path=os.path.expanduser('~')):
     # r=root, d=directories, f = files
     for r, d, f in os.walk(path):
         for file in f:
-            if '.py' in file:
+            if '.txt' in file:
                 paths.append(os.path.join(r, file))
     
     return paths
@@ -63,23 +68,23 @@ class Document:
             
             self.body = {
                 "title": self.title,
-                "text": self.text
-                "source": self.source,
+                "text": self.text,
+                "source": self.source
             }
-            print("Elasticsearch Document JSON:", self.json)
+            print("Elasticsearch Document JSON created:", self.body)
             
         except Exception as error:
             print("Document JSON instance error: ", error)
 
-    def insert_to_elastic(self.body):
-
-        slug = slugify(self.body["title"])
+    def insert(self):
+        
+        slug = slugify(self.body['title'])
 
         try:
             res = client.index(index = slug, body = self.body)
 
         except Exception as error:
-        print(f"Could not create a JSON entry for an article {slug}"
+            print(f"Could not create a JSON entry for an article {slug}")
 
 
 
@@ -97,3 +102,18 @@ def query_document(index="chatbot",id=1):
     except Exception as error:
         print ("client.index() ERROR:", error, "n")
 
+# Example document search:
+
+def search_elastic(search_term, index = ''):
+    return client.search(index = index, 
+                         body = {"query": 
+                                 {"match": 
+                                  {"text": search_term}
+                                 }
+                                }
+res = search_elastic(search_term = 'text similarity')
+
+print('Relevant articles:')
+print('===================')
+for doc in res['hits']['hits']:
+    print(doc['_source']['title'])
