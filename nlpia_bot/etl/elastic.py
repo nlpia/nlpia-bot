e7ff74020e939ebbfd23328d97a62c31f6f5cacc
hobs <hobs+github@totalgood.com>
2020-02-28 15:32:48 -0800

    cleanup elastic doctests (disabled), formatting, and unused vars
    Former-commit-id: 64a3f79d748b4fac234c563f8e1c56f9a90fe3d0

--- nlpia_bot/etl/elastic.py
+++ nlpia_bot/etl/elastic.py
@@ -1,5 +1,7 @@
+
+""" Search and scrape wikipedia articles from a chosen category """
 import os
-import json
+# import json
 from elasticsearch import Elasticsearch
 import wikipediaapi
 from slugify import slugify
@@ -11,47 +13,35 @@ except ConnectionRefusedError:
     print("Failed to launch Elstcisearch")
 
 
-''' 
-Search and scrape wikipedia articles from a chosen category
-
-'''
+log = constants.logging.getLogger(__name__)
 
 
 def print_categorymembers(categorymembers, level=0, max_level=1):
-        for c in categorymembers.values():
-            print("%s: %s (ns: %d)" % ("*" * (level + 1), c.title, c.ns))
-            if c.ns == wikipediaapi.Namespace.CATEGORY and level < max_level:
-                print_categorymembers(c.categorymembers, level=level + 1, max_level=max_level)
-
-
+    for c in categorymembers.values():
+        print("%s: %s (ns: %d)" % ("*" * (level + 1), c.title, c.ns))
+        if c.ns == wikipediaapi.Namespace.CATEGORY and level < max_level:
+            print_categorymembers(c.categorymembers, level=level + 1, max_level=max_level)
 
 
 # Save articles in separate .txt files
 
 def save_articles(path=os.path.join(constants.DATA_DIR, "wikipedia"), category='Natural_language_processing'):
-
     os.makedirs(path, exist_ok=True)
-
     wiki_wiki = wikipediaapi.Wikipedia('en')
-
     cat = wiki_wiki.page(f"Category:{category}")
 
     for key, value in cat.categorymembers.items():
         page = wiki_wiki.page(key)
         slug = slugify(key)
-        
         try:
-        
             with open(f'{path}/{slug}.txt', 'w') as new_file:
                 new_file.write(page.title)
                 new_file.write('\n')
                 new_file.write(page.fullurl)
                 new_file.write('\n')
                 new_file.write(page.text)
-                
         except Exception as error:
-            print(f"Error writing document {page.title}: ", error)
-
+            log.error(f"Error writing document {page.title}: {error}")
 
 
 def index_dir(path=os.path.join(constants.DATA_DIR, "wikipedia")):
@@ -62,59 +52,61 @@ def index_dir(path=os.path.join(constants.DATA_DIR, "wikipedia")):
         for file in f:
             if '.txt' in file:
                 paths.append(os.path.join(r, file))
-    
+
     return paths
 
 
 class Document:
-    
+
     def __init__(self, title, text, source):
 
         self.title = title
         self.text = text
         self.source = source
-        
         self.body = {}
-        
         try:
-            
             self.body = {
                 "title": self.title,
                 "text": self.text,
                 "source": self.source
             }
-            print("Elasticsearch Document JSON created:", self.body)
-            
+            log.info(f"Elasticsearch Document JSON created: {self.body}")
         except Exception as error:
-            print("Document JSON instance error: ", error)
+            log.warn(f"Document JSON instance error: ", error)
 
     def insert(self):
-        
         slug = slugify(self.body['title'])
-
+        res = None
         try:
             res = client.index(index=slug, body=self.body)
+        except Exception as e:
+            log.error(f"Could not create a JSON entry for an article {slug}. error: {e}")
+        return res
 
-        except Exception as error:
-            print(f"Could not create a JSON entry for an article {slug}")
-
-
-# Example document search:
 
 def search(text, index=''):
-    return client.search(index=index, 
-                         body={"query": 
-                                 {"match": 
-                                  {"text": text}
-                                 }
+    """ Full text search within an ElasticSearch index (''=all indexes) for the indicated text """
+    return client.search(index=index,
+                         body={"query":
+                               {"match":
+                                {"text": text}
                                 }
-    )
+                               }
+                         )
 
 
 def test_search():
     res = search(text='text similarity')
 
-    print('Relevant articles:')
-    print('===================')
+    print()
+    print('Relevant Articles')
+    print('=================')
     for doc in res['hits']['hits']:
         print(doc['_source']['title'])
+    print('=================')
+    print()
+    return res
+
+
+if __name__ == '__main__':
+    print(test_search())
