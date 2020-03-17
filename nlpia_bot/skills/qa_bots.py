bdcb16bef9f647c785606ad1cc39bb474dd8eacb
hobs <hobs+github@totalgood.com>
2020-03-16 22:56:02 -0700

    unused import fixed in qa_bots
    Former-commit-id: 7c40fdc01b27a5a64f6a548d810c5eb651437d31

--- nlpia_bot/skills/qa_bots.py
+++ nlpia_bot/skills/qa_bots.py
@@ -8,11 +8,9 @@ import zipfile
 from multiprocessing import cpu_count
 from tqdm import tqdm
 
-from constants import args  # noqa
-
 from nlpia_bot.skills.qa_models import QuestionAnsweringModel
 from nlpia_bot.etl import scrape_wikipedia
-from nlpia_bot.constants import DATA_DIR, USE_CUDA
+from nlpia_bot.constants import DATA_DIR, USE_CUDA, args  # noqa
 
 log = logging.getLogger(__name__)
 
