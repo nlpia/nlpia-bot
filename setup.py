ebd50a353d83eb870b0c2f741317f3d12e6a591f
hobs <hobs+github@totalgood.com>
2020-03-25 17:55:46 -0700

    manifest.in and setup.data_files and .package_data
    Former-commit-id: e578dcb452de99566a372fa6de5c723e3a4a8b9c

--- setup.py
+++ setup.py
@@ -18,6 +18,34 @@ except VersionConflict:
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
 
+# Mapping from package name to a list of relative path names that should be copied into the package.
+# The paths are interpreted as relative to the directory containing the package.
+package_data = {
+    'qary': [
+        'qary/data/qary.ini',
+        'qary/data/*.json',
+        'qary/data/*.csv',
+        'qary/data/*.txt',
+        'qary/data/eliza_doctor.txt',
+        'qary/data/movie_dialog.csv',
+        'qary/data/medical_sentences.json',
+        'qary/data/faq/glossary-dsdh.yml',
+        'qary/data/faq/glossary-business.yml',
+        'faq-general-python-data-science-and-machine-learning.yml',
+    ]
+}
+
+# Each (directory, files) pair in the sequence specifies the installation directory and the files to install there.
+# The directory must be a relative path (although this may change in the future, see wheel Issue #92).
+# The directory path is relative to the installation prefix (Python’s sys.prefix for a default installation; site.USER_BASE for a user installation).
+# Each file name in files is interpreted relative to the setup.py script at the top of the project source distribution.
+data_files = [
+    ('my_data', ['data/data_file'])
+]
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True, package_data={'': ['qary/data']})
+    setup(
+        use_pyscaffold=True,
+        package_data=package_data
+    )
+    # data_files=data_files)
