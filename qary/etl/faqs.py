a2b98f80850b92f6e0c8530ee8de6b6569fe1e2a
hobs <hobs+github@totalgood.com>
2020-03-29 00:41:07 -0700

    bullying faq and cleaned up faqs.py and faq_bots.py
    Former-commit-id: d3f3a14ec5a3e7c7fac067d5d97f187d053db9eb
    Former-commit-id: e8c1bab957ed69bfa3dee6b5a718819484aa7ee5

--- qary/etl/faqs.py
+++ qary/etl/faqs.py
@@ -1,10 +1,10 @@
 from tqdm import tqdm
 import yaml
 
+import numpy as np
 
 from ..constants import DEFAULT_FAQ_DOMAINS
 from ..spacy_language_model import nlp
-
 
 import logging
 log = logging.getLogger(locals().get('__name__'))
@@ -21,12 +21,21 @@ def load(domains=DEFAULT_FAQ_DOMAINS):
     >>> sorted(g['cleaned']['Allele'])
     ['acronym', 'definition', 'hashtags', 'parenthetical']
     """
-    faq_raw = {}
+    # faq_raw = {}
+    # q_vectors = pd.DataFrame()
+    questions, answers, question_vectors = [], [], []
+
     for domain in tqdm(domains):
         qa_list = yaml.load(open(f'faq-{domain}.yml'))
         for qa_dict in qa_list:
-            faq_raw = faq_raw.update({
-                qa_dict.get('Q', qa_dict.get('q', '')),
-                qa_dict.get('A', qa_dict.get('a', ''))
-            })
-    return faq_raw
+            questions.append(qa_dict.get('Q', qa_dict.get('q', '')))
+            answers.append(qa_dict.get('A', qa_dict.get('a', '')))
+            question_vectors.append(list(nlp(questions[-1]).vector))
+    # faq_raw = dict(zip(questions, answers))
+
+    # This should be a Kendra/gensim/annoy class (with methods like .find_similar)
+    return dict(
+        questions=questions,
+        answers=answers,
+        question_vectors=np.array(question_vectors)
+    )
