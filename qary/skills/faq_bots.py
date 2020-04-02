9ca1ec8b74d746e5f2327eea4de374fad7aea12d
hobs <hobs+github@totalgood.com>
2020-04-01 22:47:04 -0700

    the semantics_score and QualityScore in clibot are reducing the accuracy of the FAQ bot

--- qary/skills/faq_bots.py
+++ qary/skills/faq_bots.py
@@ -4,8 +4,8 @@ import logging
 # import pandas as pd
 
 from ..etl import faqs
-from ..constants import FAQ_DOMAINS, FAQ_MIN_SIMILARITY
-import spacy_language_model
+from ..constants import FAQ_DOMAINS, FAQ_MIN_SIMILARITY, FAQ_MAX_NUM_REPLIES
+from .. import spacy_language_model
 from ..etl import knowledge_extraction as extract  # noqa
 import numpy as np
 
@@ -39,15 +39,20 @@ class Bot:
         """ Suggest responses to a user statement string with [(score, reply_string)..]"""
         responses = []
         question_vector = nlp(statement).vector
+        log.debug(f"question_vector is {question_vector}")
         question_vector /= np.linalg.norm(question_vector)
+        log.debug(f"faq['question_vectors'].shape is {self.faq['question_vectors'].shape}")
         question_similarities = self.faq['question_vectors'].dot(question_vector.reshape(-1, 1))
         # responses.append((1.0, str(question_similarities)))
         idx = question_similarities.argmax()
-        mask = question_similarities >= FAQ_MIN_SIMILARITY
+        mask = np.array(question_similarities).flatten() >= FAQ_MIN_SIMILARITY
         if sum(mask) >= 1:
-            responses.extend(list(zip(question_similarities[mask], self.faq['answers'][mask])))
+            responses.extend(list(zip(question_similarities[mask],
+                                      (str(a) for a in self.faq['answers'][mask])
+                                      )))
+            responses = sorted(responses, reverse=True)[:FAQ_MAX_NUM_REPLIES]
         else:
             responses = [(
                 0.10,
-                f"I don't know. The only in my list of FAQs that's remotely similar to yours is \"{self.faq['questions'][idx]}\". ")]
+                f"I don't know. The only quesiton in my FAQs that is remotely similar to yours is \"{self.faq['questions'][idx]}\". ")]
         return responses
