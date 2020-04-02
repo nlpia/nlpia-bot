d2e80e2201dae1a0def085e3c0b8913b87ede7cc
hobs <hobs+github@totalgood.com>
2020-04-01 21:50:23 -0700

    worked until I added more faqs and UNKOWN_WORDVEC etc
    Former-commit-id: 0b9a1ed9324b9fb863e189dbf223e1587978d928

--- qary/skills/faq_bots.py
+++ qary/skills/faq_bots.py
@@ -1,15 +1,20 @@
 """ Pattern and template based chatbot dialog engines """
 import logging
 
-import pandas as pd
+# import pandas as pd
 
 from ..etl import faqs
-from ..constants import DEFAULT_FAQ_DOMAINS
-from .. import spacy_language_model
-from ..etl import knowledge_extraction as extract
+from ..constants import FAQ_DOMAINS, FAQ_MIN_SIMILARITY
+import spacy_language_model
+from ..etl import knowledge_extraction as extract  # noqa
+import numpy as np
 
 log = logging.getLogger(__name__)
-nlp = spacy_language_model.load()  # 'en_core_web_md'
+
+nlp = spacy_language_model.nlp
+if nlp._meta['vectors']['width'] < 300:  # len(nlp('word vector').vector) < 300:
+    log.warn(f"SpaCy Language model ({nlp._meta['name']}) doesn't contain 300D word2vec word vectors.")
+    nlp = spacy_language_model.nlp = spacy_language_model.load('en_core_web_md')
 
 
 def capitalizations(s):
@@ -24,37 +29,25 @@ class Bot:
     '`float`, `int`, `str`, and `bool`'
     """
 
-    def __init__(self,
-                 domains=DEFAULT_FAQ_DOMAINS):
+    def __init__(self, domains=FAQ_DOMAINS):
         """ Load glossary from yaml file indicated by list of domain names """
         global nlp
         self.nlp = nlp
-        self.faq = faqs.load(domains=domains)['cleaned']
-        self.vector = dict()
-        self.vector['term'] = pd.DataFrame(
-            {term: nlp(term).vector for term in self.glossary})
-        self.vector['definition'] = pd.DataFrame(
-            {term: nlp(d['definition']).vector for term, d in self.glossary.items()})
-
-        # self.synonyms = {term: term for term in self.glossary}
-        # create reverse index of synonyms to canonical terms
-        # for term, d in self.glossary.items():
-        #     self.synonyms.update(dict(zip(capitalizations(term), [term] * 4)))
-        #     acro = d['acronym']
-        #     if acro:
-        #         self.synonyms.update(dict(zip(capitalizations(acro), [term] * 4)))
+        self.faq = faqs.load(domains=domains)
 
     def reply(self, statement):
         """ Suggest responses to a user statement string with [(score, reply_string)..]"""
         responses = []
-        extracted_term = extract.whatis(statement) or ''
-        if extracted_term:
-            for i, term in enumerate(capitalizations(extracted_term)):
-                normalized_term = self.synonyms.get(term, term)
-                if normalized_term in self.glossary:
-                    responses.append((1 - .02 * i, self.glossary[normalized_term]['definition']))
+        question_vector = nlp(statement).vector
+        question_vector /= np.linalg.norm(question_vector)
+        question_similarities = self.faq['question_vectors'].dot(question_vector.reshape(-1, 1))
+        # responses.append((1.0, str(question_similarities)))
+        idx = question_similarities.argmax()
+        mask = question_similarities >= FAQ_MIN_SIMILARITY
+        if sum(mask) >= 1:
+            responses.extend(list(zip(question_similarities[mask], self.faq['answers'][mask])))
         else:
-            responses = [(0.05, "I don't understand. I can't find that question in my list of FAQa.")]
-        if not len(responses):
-            responses.append((0.25, f"My glossaries and dictionaries don't seem to contain that term ('{extracted_term}')."))
+            responses = [(
+                0.10,
+                f"I don't know. The only in my list of FAQs that's remotely similar to yours is \"{self.faq['questions'][idx]}\". ")]
         return responses
