0c75aba2e0ca76e26e2a0d2636718ea8bf27ef50
hobs <hobs+github@totalgood.com>
2020-04-07 10:22:31 -0700

    more faqs ds. a new whatmeans() question object extractor
    Former-commit-id: 473315e615a357b34e7f0cc8e9e74d80609442c6

--- qary/etl/knowledge_extraction.py
+++ qary/etl/knowledge_extraction.py
@@ -8,6 +8,24 @@ def whatis(statement):
     return extracted_term
 
 
+def whatmeans(statement):
+    match = re.match(
+        r"\b(what\s+(is|are)\s*(not|n't)?\s+(a|an|the|those|this|that)?)\s*\b"
+        r"((meaning|definition|significance|origin|interpretation|translation)[s]*\s*"
+        r"(of|for|to|in|on)?\s*(a|an|the|those|this|that)?)?[s]*\s*"
+        r"(tech|technology|science|biology|medicine|health|healthcare|genomic|data[ ]?science|machine[ ]?learning|dl|ml|ds|it|is|ir|nlp)?[es]*\s*"
+        r"(phrase|quote|term|terminology|word|token|n[-]?gram?)[es]*)\s*"
+        r"([^\?]*)(\?*)",
+        statement.lower().strip())
+    match = match or re.match(
+        r"\b(what\s+(does)\s*(a|an|the)?)\s*"
+        r"\b([^\?\s]*\s*)+"
+        r"\b(mean|signify|represent|translate|stand)?\s*(for|to|as|in)?\s*(\?*)",
+        statement.lower().strip())
+    extracted_term = match.groups()[-2] if match else ''
+    return extracted_term
+
+
 def count_upper(s):
     return sum(1 for c in s if c.isupper())
 
