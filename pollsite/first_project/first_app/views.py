dfb8f3be5d6b5e025b394e12b088a78fe120c25b
Mohammed <mms.dala@gmail.com>
2020-04-16 16:11:47 -0700

    change the skeltom
    Former-commit-id: d3b94b651908191affa289f033db901d2fde83d1

--- pollsite/first_project/first_app/views.py
+++ pollsite/first_project/first_app/views.py
@@ -1,25 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from nlpia_bot.clibot import CLIBot
 
 
 bot = CLIBot()
 
-
-def index(request, user_statement='Hello world'):
-
-    bot_reply = bot.reply(user_statement)
-
-    my_dict = {'insert_me': bot_reply}
-
-    return render(request, 'first_app/index.html', context=my_dict)
+n = ''
 
 
-def reply(request, user_statement):
+def home_view(request):
+    request.GET
+    global n
+    n = str(request.GET)
 
-    bot_reply = bot.reply(user_statement)
+    return render(request, "home.html")
 
-    my_dict = {'insert_me': bot_reply,
-               'user_statement': user_statement}
 
-    return render(request, 'first_app/index.html', context=my_dict)
+def reply(request):
+
+    bot_reply = bot.reply(n)
+
+    dict_1 = {'insert': bot_reply}
+
+    return render(request, "index.html", context=dict_1)
