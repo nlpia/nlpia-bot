277a96e749283cce871d4f780c31aa9872325f2c
hobs <hobs+github@totalgood.com>
2020-04-16 14:57:45 -0700

    fixed faq and glossary bugs

--- qary/etl/faqs.py
+++ qary/etl/faqs.py
@@ -1,15 +1,32 @@
 import os
+from pathlib import Path
 
 from tqdm import tqdm
 import yaml
+from yaml.scanner import ScannerError
 
 import numpy as np
 
-from ..constants import FAQ_DOMAINS, DATA_DIR
-from ..spacy_language_model import nlp, UNKNOWN_WORDVEC
+from qary.constants import FAQ_DOMAINS, DATA_DIR
+from qary.spacy_language_model import nlp, UNKNOWN_WORDVEC
+
 
 import logging
 log = logging.getLogger(locals().get('__name__'))
+
+
+def normalize_docvectors(docvectors):
+    """ Convert a table (2D matrix) of row-vectors into a table of normalized row-vectors """
+    docvectors = np.array(docvectors)
+    log.info(f'docvectors.shape: {docvectors.shape}')
+    norms = np.linalg.norm(docvectors, axis=1)
+    log.info(f'norms.shape: {norms.shape}')
+    norms_reshaped = norms.reshape(-1, 1).dot(np.ones((1, docvectors.shape[1])))
+    log.info(f'norms_reshaped.shape: {norms_reshaped.shape}')
+    normalized_docvectors = docvectors / norms_reshaped
+    log.info(f'normalized_docvectors.shape: {normalized_docvectors.shape}')
+    assert normalized_docvectors.shape == docvectors.shape
+    return normalized_docvectors
 
 
 def load(domains=FAQ_DOMAINS):
@@ -27,23 +44,42 @@ def load(domains=FAQ_DOMAINS):
     # q_vectors = pd.DataFrame()
     questions, answers, question_vectors = [], [], []
 
+    faqdirpath = os.path.join(DATA_DIR, 'faq')
     for domain in tqdm(domains):
-        qa_list = yaml.load(open(os.path.join(DATA_DIR, 'faq', f'faq-{domain}.yml')))
-        for qa_dict in qa_list:
-            questions.append(qa_dict.get('Q', qa_dict.get('q', '')))
-            answers.append(qa_dict.get('A', qa_dict.get('a', '')))
+        for filepath in Path(faqdirpath).glob(f'faq*{domain}*.yml'):
+            # filename = filepath.name
             try:
-                question_vectors.append(list(nlp(questions[-1] or '').vector))
-            except TypeError:
-                question_vectors.append(list(UNKNOWN_WORDVEC))
+                filepointer = open(filepath)
+            except FileNotFoundError as e:
+                log.error(f"{e}\n    Unable to find the file path object: {filepath}")
+                continue
+            with filepointer:
+                log.info(f"loading: {filepath.name}\n    with file pointer: {filepointer}")
+                try:
+                    qa_list = yaml.load(filepointer)
+                except ScannerError as e:
+                    log.error(f"{e}\n    yaml.load unable to read {filepointer.name}")
+                    continue
+            for qa_dict in qa_list:
+                questions.append(qa_dict.get('Q', qa_dict.get('q', '')))
+                answers.append(qa_dict.get('A', qa_dict.get('a', '')))
+                try:
+                    question_vectors.append(list(nlp(questions[-1] or '').vector))
+                except TypeError:
+                    question_vectors.append(list(UNKNOWN_WORDVEC))
+                    continue
+                assert len(UNKNOWN_WORDVEC) == len(question_vectors[-1])
 
-    question_vectors = np.array(question_vectors)
-    question_vectors /= np.linalg.norm(
-        question_vectors, axis=1).reshape(-1, 1).dot(np.ones((1, question_vectors.shape[1])))
+    log.debug(f'len(question_vectors): {len(question_vectors)}')
+
     questions = np.array(questions)
+    log.debug(f'len(questions): {len(questions)}')
     answers = np.array(answers)
+    log.debug(f'len(answers): {len(answers)}')
     mask = np.array([(bool(a) and bool(q) and len(str(a).strip()) > 0 and len(str(q).strip()) > 0)
                      for a, q in zip(questions, answers)])
+
+    question_vectors = normalize_docvectors(question_vectors)
 
     # This should be a Kendra/gensim/annoy class (with methods like .find_similar)
     return dict(
