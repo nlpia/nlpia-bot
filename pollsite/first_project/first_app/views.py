305b7a81dff7a03695e7984c8c264f2ad9554ff3
Mohammed <mms.dala@gmail.com>
2020-04-17 11:37:15 -0700

    fix the Query.dict

--- pollsite/first_project/first_app/views.py
+++ pollsite/first_project/first_app/views.py
@@ -1,24 +1,40 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from . import models
 from nlpia_bot.clibot import CLIBot
+import re
 
 
 bot = CLIBot()
 
-n = ''
+get_request = None
 
 
 def home_view(request):
-    request.GET
-    global n
-    n = str(request.GET)
 
-    return render(request, "home.html")
+    # function return Query.dict from GET
+    global get_request
+    get_request = request.GET
+
+    return render(request, "question.html", )
 
 
 def reply(request):
 
-    bot_reply = bot.reply(n)
+    # k = str(n[20:-4])
+    bot_reply = bot.reply(get_request['n'])
 
-    dict_1 = {'insert': bot_reply}
+    dict_1 = {'insert': bot_reply, 'Question': get_request['n']}
 
-    return render(request, "index.html", context=dict_1)
+    return render(request, "answer.html", context=dict_1)
+
+
+class PostListView(ListView):
+    context_object_name = 'posts'
+    model = models.Post
+
+
+class PostDetailView(DetailView):
+    context_object_name = 'post_detail'
+    model = models.Post
+    template_name = 'first_app/first_app_detail.html'
