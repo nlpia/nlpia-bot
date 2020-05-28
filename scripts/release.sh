322ced83bdae8d9c88412210f5a8562166db71c1
hobs <hobs+github@totalgood.com>
2020-05-27 22:59:14 -0700

    release.sh and setup_dev_env.sh chmod +x
    Former-commit-id: 5913f0396f27791ea6d528caed1650f2ea2d9695

---
+++ scripts/release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# release.sh
+
+# pip install -U twine wheel setuptools
+
+git tag -l
+git tag -a "$1" -m "$2"
+python setup.py sdist
+python setup.py bdist_wheel
+twine check dist/*
+twine uplaod dist/"qary-$1"*
