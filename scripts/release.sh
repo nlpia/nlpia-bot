aa793c4e834b267d435ab5d1d894169b5e4e41ea
hobs <hobs+github@totalgood.com>
2020-05-27 23:04:19 -0700

    add commit -am and push --tag to release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,8 +3,10 @@
 
 # pip install -U twine wheel setuptools
 
+git commit -am "$2"
 git tag -l | cat
 git tag -a "$1" -m "$2"
+git push --tag
 python setup.py sdist
 python setup.py bdist_wheel
 twine check dist/*
