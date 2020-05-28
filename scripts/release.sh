486f9de89a9b9e307b384d8cd9ae59e260040508
hobs <hobs+github@totalgood.com>
2020-05-27 23:06:17 -0700

    more release.sh automagic git tag push
    Former-commit-id: 3761f13a817f9a000a9cc9e1c5f86ab881ecfcb1
    Former-commit-id: c45b4c30c81a75fd32bc67557115061ea22dfeb4

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,10 +4,11 @@
 # pip install -U twine wheel setuptools
 
 git commit -am "$2"
+git push
 git tag -l | cat
 git tag -a "$1" -m "$2"
-git push --tag
 python setup.py sdist
 python setup.py bdist_wheel
 twine check dist/*
 twine upload dist/"qary-$1"*
+git push --tag
