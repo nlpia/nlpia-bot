6b5fd2ffe3cbc9fbcd99553dc8f7aa0a0b7915e4
Hobson Lane <hobs+gitlab@totalgood.com>
2020-06-12 10:54:08 -0700

    cant import fileutils anywhere except constants!

--- qary/etl/netutils.py
+++ qary/etl/netutils.py
@@ -5,7 +5,7 @@ import os
 import urllib
 from tqdm import tqdm
 
-from qary.constants import LARGE_FILES
+from qary.constants import LARGE_FILES, DATA_DIR
 
 
 log = logging.getLogger(__name__)
@@ -28,8 +28,9 @@ def download_if_necessary(
     dest_path = dest_path or file_meta.get('path')
     if not dest_path:
         file_meta = LARGE_FILES.get(url_or_name, LARGE_FILES.get(url, {}))
+        filename = urllib.Path(file_meta.get('filename', dest_path)).name
         dest_path = file_meta.get('path', os.path.join(DATA_DIR, filename))
-    filename = file_meta.get('filename', dest_path)
+    filename = file_meta.get('filename', urllib.Path(dest_path).name)
 
     with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=filename) as dpb:
         urllib.request.urlretrieve(url, filename=dest_path, reporthook=dpb.update_to)
