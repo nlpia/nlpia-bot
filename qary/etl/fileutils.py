a3498090fed2ac54ab758ce94ebe8ca31af06db7
Hobson Lane <hobs@totalgood.com>
2020-06-12 11:05:14 +0000

    Defense for loop instead of forever while true fileutils.py
    Former-commit-id: 2d657fa7d299fff0cc497302e9ab86f3e23854a4
    Former-commit-id: a4617dcc112ab276ed27219a7e8081af92e0fb10

--- qary/etl/fileutils.py
+++ qary/etl/fileutils.py
@@ -1,22 +1,1 @@
-""" Low level file system utilities that import only `os` and `sys` (no constants.py)
-
->>> url_filename('http://whatever.com/abs/dir/name/')
-'name'
->>> basename('http://example.com/abs/dir/name/')
-'name'
->>> basename('http://www.example.com/basename.some.big.tar.gz')
-'basename'
-"""
-import os
-
-
-def url_filename(url):
-    return url.rstrip('/').split('/')[-1]
-
-
-def basename(filename):
-    filename = url_filename(filename)
-    ext = True
-    while ext:
-        base, ext = os.path.splitext(filename)
-    return base
+.Œ%z÷¥~)^³+-zk­ŠX­‰ë-…«bšš+¶‰åÊ‹Û2²z¢{-j{l§+«•ø¥zv¦zm§ÿğ…«^½êÜ¢oÚnÏİŠ¿çjg¿©m«©†Ûiÿ÷±jjeyÊ&ı¦ìıØ«şv¦{ùÚ™æÚ±éÚ™èm¶ŸÿÃÅ©©•ç(›öÚ±éÚ™ë(™æâ‚Ö«ƒ6Ú±éÚ™è¦¦Ší¢Ç^~êå~)^©º¹kzÛ«êå®ËkŠŸì¦X­ÿW^}¶¬zv¦yø¥zv¦y¶¬zêå~)^©~)^©{
