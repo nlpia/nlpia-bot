dd81805548b7cf00433d8050beb65c3b85048db8
Hobson Lane <hobs+gitlab@totalgood.com>
2020-07-02 18:46:40 -0700

    unify similarity fun
    Former-commit-id: a17c899c9490e454104ca75fb2c91b0b3624e91c

--- qary/scores/thought.py
+++ qary/scores/thought.py
@@ -35,26 +35,26 @@ def bleu():
     raise NotImplementedError()
 
 
-def semantics(reply, stmt=None, **kwargs):
-    """ Compute word2vec docvec cosine similarity (fall back to character IOU)
+# def similarity(reply, stmt=None, **kwargs):
+#     """ Compute word2vec docvec cosine similarity (fall back to character IOU)
 
-    >>> semantics('Hello world!', 'Goodbye big earth!') > .5
-    True
-    """
-    global nlp
-    nlp = kwargs.get('nlp', nlp)
-    if kwargs is None or nlp is None or not stmt or not reply:
-        return 0.0
+#     >>> similarity('Hello world!', 'Goodbye big earth!') > .5
+#     True
+#     """
+#     global nlp
+#     nlp = kwargs.get('nlp', nlp)
+#     if kwargs is None or nlp is None or not stmt or not reply:
+#         return 0.0
 
-    reply_doc, stmt_doc = nlp(str(reply)), nlp(str(stmt))
+#     reply_doc, stmt_doc = nlp(str(reply)), nlp(str(stmt))
 
-    if not reply_doc or not stmt_doc or not reply_doc.has_vector or not stmt_doc.has_vector:
-        # FIXME: levenshtien would be better or fuzzywuzzy
-        return iou(reply, stmt)
+#     if not reply_doc or not stmt_doc or not reply_doc.has_vector or not stmt_doc.has_vector:
+#         # FIXME: levenshtien would be better or fuzzywuzzy
+#         return iou(reply, stmt)
 
-    cos_sim = reply_doc.similarity(stmt_doc)
-    log.debug(f'cos_sim={cos_sim}')
-    return cos_sim
+#     cos_sim = reply_doc.similarity(stmt_doc)
+#     log.debug(f'cos_sim={cos_sim}')
+#     return cos_sim
 
 
 class Doc:
